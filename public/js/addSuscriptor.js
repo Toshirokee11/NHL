850dcd0eb00cea494614c1a5862222880a05dfd7
codxut <miguelespiritu1999@gmail.com>
2021-08-25 12:01:12 -0500

    mostrar modal cupon dsct.

--- public/js/addSuscriptor.js
+++ public/js/addSuscriptor.js
@@ -27,7 +27,9 @@ $("#login").submit(function(e){
                 if(data == true){
                     //alert("cookie agregada usuario");
                     document.cookie = "user=true; max-age=3600; path=/";
+                    document.cookie = "modal=1; max-age=2; path=/";
                     location.reload();
+                    
                 }else{
                     alert("error");
                 }
@@ -38,3 +40,13 @@ $("#login").submit(function(e){
     }
 
 });     
+
+$(document).ready(() => {
+    if (document.cookie.includes("user")) {
+        $('#modal .contenedor:nth-child(1)').removeClass('activo').addClass('inactivo');
+        $('#modal .contenedor:nth-child(2)').removeClass('inactivo').addClass('activo');
+        if (document.cookie.includes("modal")) {
+            $('#catalogo').click();
+        }
+    }
+})
